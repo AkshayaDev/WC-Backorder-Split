570904940a896cd8329a898900a89dec1a00d48b
Akshaya Swaroop <akshaya.swaroop3291@gmail.com>
2019-02-21 16:28:35 +0530

    Backorder table added

--- classes/class-wc-backorder-split-frontend.php
+++ classes/class-wc-backorder-split-frontend.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * WC_Backorder_Split_Frontend Class
+ *
+ * Split the order if it doesn't have enough products in stock
+ *
+ * @author      Akshaya Swaroop
+ * @package     wc-backorder-split/classes
+ * @since       1.0
+ */
 
 if(!defined('ABSPATH')) exit; // Exit if accessed directly
 
@@ -18,8 +27,16 @@ class WC_Backorder_Split_Frontend {
 
 		add_filter( 'bulk_actions-edit-shop_order', array($this,'wcbs_get_custom_order_status_bulk'));
 
+		add_action( 'woocommerce_order_details_after_order_table', array($this,'wcbs_render_backorder_table') );
+
 	}
 
+	/**
+	 * Check if order contains backorder products.
+	 *
+	 * @param  int  $order_id Order ID.
+	 * @return bool
+	 */
 	public function wcbs_order_contains_backorder_products($order_id) {
 		if ( ! $order = wc_get_order( $order_id ) ) {
 			return false;
@@ -38,6 +55,13 @@ class WC_Backorder_Split_Frontend {
 		return false;
 	}
 
+
+	/**
+	 * Create backorder from parent order.
+	 *
+	 * @param  int  $parent_order_id Parent Order ID.
+	 * @return WC_Order|bool Order object if successful or false.
+	 */
 	public function wcbs_create_backorder($parent_order_id) {
 
 		if ( ! $parent_order = wc_get_order( $parent_order_id ) ) {
@@ -57,6 +81,13 @@ class WC_Backorder_Split_Frontend {
 		return wc_create_order( $default_args );
 	}
 
+	/**
+	 * Add customer data to backorder.
+	 *
+	 * @param  int  $parent_order_id Parent Order ID.
+	 * @param WC_Order $backorder Order object.
+	 * @return void
+	 */
 	public function wcbs_add_customer_data($parent_order_id,$backorder) {
 
 		if ( ! $parent_order = wc_get_order( $parent_order_id ) ) {
@@ -98,6 +129,14 @@ class WC_Backorder_Split_Frontend {
 		}
 	}
 
+	/**
+	 * Review and split the orginal order.
+	 *
+	 * @param  int  $order_id Order ID.
+	 * @param array $posted_data Checkout form posted data.
+	 * @param WC_Order $order Order object.
+	 * @return void
+	 */
 	public function wcbs_review_and_split_order($order_id, $posted_data, $order) {
 
 		if ( ! $order = wc_get_order( $order_id ) ) {
@@ -140,14 +179,14 @@ class WC_Backorder_Split_Frontend {
 
 			if ( $product && $product->backorders_allowed() ) {
 
-				if($product->is_on_backorder( $item->get_quantity() )) {
+				if($product->is_on_backorder( $item->get_quantity() ) && ($product->get_stock_quantity() <=0)) {
 					$backorder->add_product( $product, $item->get_quantity() );
 					$order->remove_item( $item_id );
 				}else{
 					$backordered_quantity = $item->get_quantity() - max( 0, $product->get_stock_quantity() );
 
-					$item->set_quantity($product->get_stock_quantity());
-					$item->save();
+					$order->remove_item( $item_id );
+					$order->add_product( $product, $product->get_stock_quantity());
 
 					if($backorder && !empty($backordered_quantity)) {
 						$backorder->add_product( $product, $backordered_quantity );
@@ -187,7 +226,8 @@ class WC_Backorder_Split_Frontend {
 			$backorder->save();
 
 			// update order post meta
-			update_post_meta($order_id,'_backorder_created','yes');
+			update_post_meta($order_id,'_backorder_created', 'yes');
+			update_post_meta($order_id,'_backorder_id', $backorder->get_id());			
 
 			update_post_meta($backorder->get_id(), '_initial_order_status', 'backordered');
 			$order->add_order_note( sprintf( __( 'This order has backordered items and has been split into Order #%s.', 'woocommerce' ), $backorder->get_id() ) );
@@ -196,12 +236,24 @@ class WC_Backorder_Split_Frontend {
 
 	}
 
+	/**
+	 * Add new order status in WC
+	 *
+	 * @param array $order_statuses
+	 * @return array
+	 */
 	public function wcbs_add_backorder_order_status($order_statuses) {
 		global $WC_Backorder_Split;
 		$order_statuses['wc-backordered'] = _x( 'Backordered', 'Order status', $WC_Backorder_Split->text_domain );
 		return $order_statuses;
 	}
 
+	/**
+	 * Update backorder status on status change
+	 *
+	 * @param int $order_id
+	 * @return void
+	 */
 	public function wcbs_update_backorder_order_status($order_id) {
 		$order = wc_get_order( $order_id );
 
@@ -219,6 +271,12 @@ class WC_Backorder_Split_Frontend {
 
 	}
 
+	/**
+	 * Register new order status in WC
+	 *
+	 * @param array $order_statuses
+	 * @return array
+	 */
 	public function wcbs_register_new_order_status($order_statuses) {
 		global $WC_Backorder_Split;
 		// Status must start with "wc-"
@@ -234,10 +292,87 @@ class WC_Backorder_Split_Frontend {
 		return $order_statuses;
 	}
 
+	/**
+	 * Add new order status in bulk actions (Order's Listing)
+	 *
+	 * @return array
+	 */
 	public function wcbs_get_custom_order_status_bulk() {
 		// Note: "mark_" must be there instead of "wc"
 		$bulk_actions['mark_backordered'] = 'Change status to backordered';
 		return $bulk_actions;		
-	}	
+	}
+
+	/**
+	 * Add new backorder table in thank you page.
+	 *
+	 * @param WC_Order $order Order object.
+	 */
+	public function wcbs_render_backorder_table($order) {
+
+		if(!$order) {
+			return;
+		}
+
+		if ( get_post_meta( $order->get_id(), '_backorder_id', true ) ) :
+			$backorder = wc_get_order(get_post_meta( $order->get_id(), '_backorder_id', true ));
+
+			if(!$backorder) {
+				return;
+			}
+
+			$order_items = $backorder->get_items( 'line_item' );
+			$show_purchase_note = $backorder->has_status( array( 'completed', 'processing' ) );			
+
+		?>
+		<h2 class="woocommerce-order-details__title"><?php _e( 'Backorder details', 'woocommerce' ); ?></h2>
+
+		<table class="woocommerce-table woocommerce-table--order-details shop_table order_details">
+
+		<thead>
+			<tr>
+				<th class="woocommerce-table__product-name product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
+				<th class="woocommerce-table__product-table product-total"><?php _e( 'Total', 'woocommerce' ); ?></th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php
+			foreach ( $order_items as $item_id => $item ) {
+				$product = $item->get_product();
+
+				wc_get_template( 'order/order-details-item.php', array(
+					'order'			     => $backorder,
+					'item_id'		     => $item_id,
+					'item'			     => $item,
+					'show_purchase_note' => $show_purchase_note,
+					'purchase_note'	     => $product ? $product->get_purchase_note() : '',
+					'product'	         => $product,
+				) );
+			}
+			?>
+		</tbody>
+
+		<tfoot>
+			<?php
+				foreach ( $backorder->get_order_item_totals() as $key => $total ) {
+					?>
+					<tr>
+						<th scope="row"><?php echo $total['label']; ?></th>
+						<td><?php echo $total['value']; ?></td>
+					</tr>
+					<?php
+				}
+			?>
+			<?php if ( $backorder->get_customer_note() ) : ?>
+				<tr>
+					<th><?php _e( 'Note:', 'woocommerce' ); ?></th>
+					<td><?php echo wptexturize( $backorder->get_customer_note() ); ?></td>
+				</tr>
+			<?php endif; ?>
+		</tfoot>
+		</table>
+	<?php endif;
+	}
 
 }
